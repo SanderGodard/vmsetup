81f716f14e8bf7a487d9904b45ce147b030fe654
sanderjobb <sandermenjobb@gmail.com>
2025-09-09 15:09:00 +0200

    Typo fix

--- xfce4-customize.sh
+++ xfce4-customize.sh
@@ -3,7 +3,7 @@
 # Setting wallpaper for XFCE4
 echo -e "Setting up wallpaper"
 xrandr --listmonitors | awk '{print $4}' | tail -n $(( $( xrandr --listmonitors | wc -l ) - 1 )) | while read monitor; do
-    xfconf-query --channel xfce4-desktop -property /backdrop/screen0/monitor"$monitor"/workspace0/last-image -n -t string -s "$HOME"/Pictures/wp.jpg
+    xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor"$monitor"/workspace0/last-image -n -t string -s "$HOME"/Pictures/wp.jpg
 done
 
 echo -e "Finished"
