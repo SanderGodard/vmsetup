78c3848b4363c0411b1ac3d7a098fbd2e497ba78
sanderjobb <sandermenjobb@gmail.com>
2025-09-09 15:02:50 +0200

    Updated wallpaper-section in post deploy

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -17,15 +17,6 @@ fi
 # Set bash as default shell
 chsh -s /usr/bin/bash
 
-# Setting wallpaper
-echo -e "Setting up wallpaper"
-monitor=$(xfconf-query --channel xfce4-desktop --list | grep -i last-image | grep workspace)
-xfconf-query -c xfce4-desktop -p "$monitor" -s "$HOME"/Pictures/wp.jpg
-
-xrandr --listmonitors | awk '{print $4}' | tail -n $(( $( xrandr --listmonitors | wc -l ) - 1 )) | while read monitor; do
-    xfconf-query --channel xfce4-desktop -property /backdrop/screen0/monitor"$monitor"/workspace0/last-image -n -t string -s "$HOME"/Pictures/wp.jpg
-done
-
 # Keyboard settings
 setxkbmap no
 localectl set-x11-keymap no # Asks for system to input password
@@ -37,6 +28,7 @@ chmod +x ~/.prompt_script
 echo "Using ./generate-symlinks.sh to fix symlinks that dotter cannot"
 $(./generate-symlinks.sh) 2> /dev/null
 
+echo "Use ./xfce4-customize.sh to set wallpaper and such"
 echo "Remember to update and upgrade"
 
 echo "[-] Opening bash" 
